8dff9ee57e7d3efc32725da703cef670d30178db
Ahmed Amin <120580570+AhmedAminIV@users.noreply.github.com>
2025-10-16 21:54:04 +0300

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,11 +1,8 @@
 def call(String image){
-    pipeline {
-        agent any
-        stages {
-            stage('docker_push') { 
-                steps {
-                    sh 'docker push ${image}'
-                }
+    stages {
+        stage('docker_push') { 
+            steps {
+                sh 'docker push ${image}'
             }
         }
     }
